d5f7a9166c24ef823acc2e1b03b9ed667943e0b6
Jaskirat-Kaur-Gill <everythingrene@gmail.com>
2025-11-04 13:38:45 +1100

    Update upload.py

--- api/upload.py
+++ api/upload.py
@@ -23,8 +23,6 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://127.0.0.1:5500",
-        "http://localhost:5500",
         "https://inkinsights.vercel.app",
     ],
     allow_credentials=True,
